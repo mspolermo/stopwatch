d394be7f069f8e13b4868d8cad68673ba93b86fb
mspolermo <mspolermo@gmail.com>
2023-01-16 15:14:19 +0500

    Version 1.01

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ let timerId;
 let startLapValue 
 let currentLapValue='00:00:00';
 let lapResult;
+let lapResultUpg = [];
+let clicks = 0;
 
 
 //Запуск секундомера по кнопке "Старт"
@@ -50,9 +52,11 @@ btnReset.addEventListener('click', function reset(){
   miliSecCounter = 0;
   secCounter = 0;
   minCounter = 0;
-  counterElement.innerText = '00:00:00';
+  clicks = 0;
   currentLapValue='00:00:00';
   startLapValue = [00, 00, 00];
+  lapResultUpg = [00, 00, 00];
+  counterElement.innerText = '00:00:00';
   const htmlResult = `<p>Результат: ${result}</p>`;
 container.insertAdjacentHTML('beforeend', htmlResult);
 });
@@ -60,9 +64,11 @@ container.insertAdjacentHTML('beforeend', htmlResult);
 //Реализация кнопки "Круг"
 const btnLap = document.querySelector('#lap');
 btnLap.addEventListener('click', function lap() {
-  if (currentLapValue == '00:00:00') {
-  startLapValue = [00, 00, 00];
-} ;
+  
+clicks += 1; //Счетчик нажатий
+
+if (currentLapValue == '00:00:00') {startLapValue = [00, 00, 00];};
+
 currentLapValue = result.split(":");
 let mil1 = Number(startLapValue[2]) + Number(startLapValue[1])*100 + Number(startLapValue[0]*6000);
 let mil2 = Number(currentLapValue[2]) + Number(currentLapValue[1])*100 + Number(currentLapValue[0]*6000);
@@ -70,19 +76,23 @@ let razn = Math.abs(mil1-mil2);
 lapResult = [Math.floor(razn/6000),Math.floor((razn-Math.floor(razn/6000)*6000)/100),
 razn-((Math.floor(razn/6000)*6000)+(Math.floor((razn-Math.floor(razn/6000)*6000)/100))*100)];
 startLapValue=currentLapValue;
-let lapResultUpg = [];
+
 (lapResult[0]<10) ? lapResultUpg[0] = '0'+lapResult[0] : lapResultUpg[0]=lapResult[0];
 (lapResult[1]<10) ? lapResultUpg[1] = '0'+lapResult[1] : lapResultUpg[1]=lapResult[1];
 (lapResult[2]<10) ? lapResultUpg[2] = '0'+lapResult[2] : lapResultUpg[2]=lapResult[2];
+
 //Вставка результата 
 let htmlResult;
-if ( miliSecCounter == 0 && secCounter == 0 && minCounter == 0){
+if (miliSecCounter == 0 && secCounter == 0 && minCounter == 0){
   htmlResult = `<p>Секундомер не запущен</p>`;
   container.insertAdjacentHTML('beforeend', htmlResult);
-} else {
-  htmlResult = `<p>Круг: ${lapResultUpg.join(":")} Общий результат: ${currentLapValue.join(":")} </p>`;
-container.insertAdjacentHTML('beforeend', htmlResult);
-//Счетчик нажатий
+  clicks = 0;
+  currentLapValue='00:00:00';
+  startLapValue = [00, 00, 00];
+  lapResultUpg = [00, 00, 00];
+} 
+else {
+  htmlResult = `<p>#${clicks} Круг: ${lapResultUpg.join(":")} Общий результат: ${currentLapValue.join(":")} </p>`;
+  container.insertAdjacentHTML('beforeend', htmlResult);
 };
-
 }); 
